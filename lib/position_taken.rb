e10810269411c6474eb804f81f19b8a43b66d927
Devin Wilson <craigvin84@yahoo.com>
2018-01-13 22:52:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,2 @@
 def position_taken?(board, index)
-  if position_taken?(board, index) == " "
-    return false
-  end
-  if position_taken?(board, index) == "X" && position_taken?(board, index) == "Y"
-    return true
-  end
 end
