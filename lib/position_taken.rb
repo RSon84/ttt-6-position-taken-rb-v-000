61f0d32d315240e4b395e77404fa4db7e5f253e4
Devin Wilson <craigvin84@yahoo.com>
2018-01-13 23:47:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-  if board[0, 1, 2, 3, 4, 5, 6, 7, 8] != ("X" || "Y")
+  if board["0", "1", "2", "3", "4", "5", "6", "7", "8"] != ("X" || "Y")
     return false
-  else board[0, 1, 2, 3, 4, 5, 6, 7, 8] == ("X" || "Y")
+  else board["0", "1", "2", "3", "4", "5", "6", "7", "8"] == ("X" || "Y")
     return true
   end
 end
