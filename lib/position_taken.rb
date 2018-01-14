244d25688c289c75930201615af503bac36783ab
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 00:33:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] != ("X" || "O")
     return false
-  elsif board[index] == ("X" || "O")
+  elsif board[index] == ("X") || board[index] == ("O")
     return true
   end
 end
